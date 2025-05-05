6490ecac2b7e24ef0e351b06953b1c0fa02fa998
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-05 21:46:20 +0530

    Create first component

--- React/shopapp/src/App.js
+++ React/shopapp/src/App.js
@@ -1,10 +1,11 @@
-// import logo from './logo.svg';
 import './App.css';
+import Item from './components/item';
 
 function App() {
   return (
-    <div className="App">
-      Hello React
+    <div>
+      <div className="App">Hello React</div>
+      <Item></Item>
     </div>
   );
 }
---
+++ React/shopapp/src/components/item.js
@@ -0,0 +1,11 @@
+import './item.css'
+
+function Item() {
+    return (
+        <p className="header">
+            New Header
+        </p>
+    )
+}
+
+export default Item;
