cdaf8141bc3c9c8ec5afa3ddc9c56d4e4487c0df
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-10 14:37:35 +0530

    Addin form

--- React/shopapp/src/components/ProductForm.js
+++ React/shopapp/src/components/ProductForm.js
@@ -1,19 +1,39 @@
+import { useState } from 'react';
 import './ProductForm.css';
 
 function ProductFrom() {
+
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState('');
+    const [date, setDate] = useState('');
+
+    function titleChangeHandler(event) {
+        setTitle(event.target.value);
+    }
+
+    function priceChangeHandler(event) {
+        setPrice(event.target.value);
+    }
+
+    function dateChangeHandler(event) {
+        setDate(event.target.value);
+
+        console.log([title, price, date])
+    }
+    
     return(
         <form>
             <div className='prod-label'>
                 <label>Title</label>
-                <input type='text'></input>
+                <input type='text' onChange={titleChangeHandler}></input>
             </div>
             <div className='prod-label'>
                 <label>Price</label>
-                <input type='text'></input>
+                <input type='text' onChange={priceChangeHandler}></input>
             </div>
             <div className='prod-label'>
                 <label>Mfg Date</label>
-                <input type='date' min="2025-05-10" max="2026-05-10"></input>
+                <input type='date' onChange={dateChangeHandler}></input>
             </div>
             <div className='prod-button'>
                 <button type='submit'>Add Product</button>
