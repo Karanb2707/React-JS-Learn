f277efb514c19c56758564016d9706de9ad25aef
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-10 12:13:05 +0530

    Basic product str done

--- React/shopapp/src/components/Products.js
+++ React/shopapp/src/components/Products.js
@@ -4,14 +4,19 @@ import ProductItem from './ProductItem'
 const Products = (props) => {
     return (
         <div className="products">
+            
             <ProductItem 
                 title={props.items[0].title} 
                 price={props.items[0].price}
             >
             </ProductItem>
-            <ProductItem title={props.items[1].title}>
+
+            <ProductItem 
+                title={props.items[1].title}>
             </ProductItem>
-            <ProductItem title={props.items[2].title}>
+
+            <ProductItem 
+                title={props.items[2].title}>
             </ProductItem>
         </div>
     )
