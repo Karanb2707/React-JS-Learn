3b506626d42844c14574ff3407897c6d5ea41617
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-10 13:39:10 +0530

    useState use

--- React/shopapp/src/components/ProductItem.js
+++ React/shopapp/src/components/ProductItem.js
@@ -1,16 +1,27 @@
+import React, {useState} from 'react';
+
 import './ProductItem.css';
 import ProductPriceDate from './ProductPriceDate'
 
 const ProductItem = (props) => {
+
+    const [title, setTitle] = useState(props.title);
     
+    // useState function need 2 things:
+    // one is used to set default value
+    // second is updating function which update
+
     function clickHandler() {
-        alert('Added in Cart!!')
+        setTitle("Added!");
+        console.log('Added in Cart!!')
     }
 
     return(
         <div className = "productItem">
-            <ProductPriceDate price={props.price} date={props.date}></ProductPriceDate>
-            <h1 className='prod-title'>{props.title}</h1>
+            <ProductPriceDate price={props.price} date={props.date}/>
+
+            <h1 className='prod-title'>{title}</h1>
+
             <button onClick={clickHandler}>Add to Cart</button>
         </div>
     )
