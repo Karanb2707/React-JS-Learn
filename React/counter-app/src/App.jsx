ae37015d5b1fded12fe0402b5cb0b1d4af389e8d
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-11 00:20:10 +0530

    New project

---
+++ React/counter-app/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <div>Increment & Decrement</div>
+
+      <div>
+        <button>-</button>
+        <div></div>
+        <button>+</button>
+      </div>
+
+      <button>Reset</button>
+    </div>
+  );
+}
+
+export default App
