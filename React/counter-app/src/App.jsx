bc9e272ad3b04cf870b7a530138598d870a46743
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-11 13:47:29 +0530

    Added css

--- React/counter-app/src/App.jsx
+++ React/counter-app/src/App.jsx
@@ -2,16 +2,25 @@
 function App() {
 
   return (
-    <div className="w-[100vw] h-[100vh]">
-      <div>Increment & Decrement</div>
+    <div className="w-[100vw] h-[100vh] flex flex-col gap-8 bg-amber-200 items-center justify-center">
+      <div className="text-4xl font-semibold">Increment & Decrement</div>
 
-      <div>
-        <button>-</button>
-        <div></div>
-        <button>+</button>
+      <div className="w-[25%] flex justify-center border-2 bg-white border-red-900 gap-1 rounded-md">
+        <button className="w-full py-2.5 m-1 border text-xl font-extrabold bg-slate-200 rounded-sm cursor-pointer">
+          –
+        </button>
+        <div className="w-full text-center text-3xl place-content-center font-semibold">
+          0
+        </div>
+        <button className="w-full py-2.5 m-1 border text-xl font-extrabold bg-slate-200 rounded-sm cursor-pointer">
+          +
+        </button>
       </div>
 
-      <button>Reset</button>
+      <button className="w-[10%] p-3 border bg-red-500 text-white font-semibold rounded-xl cursor-pointer
+        hover:shadow-sm shadow-red-900 hover:border-red-900">
+        Reset
+      </button>
     </div>
   )
 }
