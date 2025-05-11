05bcfc30629b3b2fb0c54cba5a768f85d008b1da
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-11 18:59:27 +0530

    counter app done

--- React/counter-app/src/App.jsx
+++ React/counter-app/src/App.jsx
@@ -1,23 +1,38 @@
+import { useState } from "react"
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function decHandler() {
+    setCount(count - 1);
+  }
+
+  function incHandler() {
+    setCount(count + 1);
+  }
+
+  function resetHandler() {
+    setCount(0);
+  }
 
   return (
     <div className="w-[100vw] h-[100vh] flex flex-col gap-8 bg-amber-200 items-center justify-center">
       <div className="text-4xl font-semibold">Increment & Decrement</div>
 
       <div className="w-[300px] flex justify-center border-2 bg-white border-red-900 gap-1 rounded-md">
-        <button className="w-full py-2.5 m-1 border text-xl font-extrabold bg-slate-200 rounded-sm cursor-pointer">
+        <button onClick={decHandler} className="w-full py-2.5 m-1 border text-xl font-extrabold bg-slate-200 rounded-sm cursor-pointer">
           –
         </button>
         <div className="w-full text-center text-3xl place-content-center font-semibold">
-          0
+          {count}
         </div>
-        <button className="w-full py-2.5 m-1 border text-xl font-extrabold bg-slate-200 rounded-sm cursor-pointer">
+        <button onClick={incHandler} className="w-full py-2.5 m-1 border text-xl font-extrabold bg-slate-200 rounded-sm cursor-pointer">
           +
         </button>
       </div>
 
-      <button className="w-[120px] p-3 bg-red-500 text-white font-semibold rounded-xl cursor-pointer
+      <button onClick={resetHandler} className="w-[120px] p-3 bg-red-500 text-white font-semibold rounded-xl cursor-pointer
         hover:shadow-sm shadow-red-900 hover:border border-red-900">
         Reset
       </button>
