099ae734fcd7c2b2de84d469a207996fc9db3736
Karan Bolake <95834281+Karanb2707@users.noreply.github.com>
2025-05-15 13:14:54 +0530

    Update Tours.jsx

--- React/tour-app/src/components/Tours.jsx
+++ React/tour-app/src/components/Tours.jsx
@@ -3,11 +3,11 @@ import Card from "./Card";
 function Tours({tours, removeTourFun}) {
     return (
         <div>
-            <div>
+            <div className="text-center font-bold text-3xl p-9">
                 <h1>Plan With Love</h1>
             </div>
 
-            <div>
+            <div className="w-[90%] grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-center mx-auto mb-[120px]">
                 {
                     tours.map( tour => {
                         return <Card key={tour.id} {...tour} removeTour = {removeTourFun}></Card>
