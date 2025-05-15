505da561b44ee59f7378e139314cf969fb0e009f
Karan Bolake <95834281+Karanb2707@users.noreply.github.com>
2025-05-15 13:13:26 +0530

    Update App.jsx

--- React/tour-app/src/App.jsx
+++ React/tour-app/src/App.jsx
@@ -13,9 +13,12 @@ const App = () => {
 
   if(tours.length === 0) {
     return (
-      <div>
-        <h2>No Tours Left</h2>
-        <button onClick={() => setTours(data)}>Refresh</button>
+      <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center">
+        <h2 className="text-3xl font-semibold p-2">No Tours Left</h2>
+        <button className="w-[160px] border rounded-2xl m-2 py-2 text-xl text-white bg-red-900 font-semibold cursor-pointer" 
+          onClick={() => setTours(data)}>
+          Refresh
+        </button>
       </div>
     )
   }
