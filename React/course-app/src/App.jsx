b375f71b68038458fb5c9fd9be7c942d4e19d64b
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-19 01:08:01 +0530

    Done with btn logic

--- React/course-app/src/App.jsx
+++ React/course-app/src/App.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Filter from './components/Filter'
 import Cards from './components/Cards'
+import { filterData} from "./data"
+
+// apiUrl
 
 const App = ()  => {
   return (
     <div>
       <Navbar/>
-      <Filter/>
+      <Filter filterData = {filterData}/>
       <Cards/>
     </div>
   );
 };
 
-// export const apiUrl = "https://codehelp-apis.vercel.app/api/get-top-courses";
 
 export default App
